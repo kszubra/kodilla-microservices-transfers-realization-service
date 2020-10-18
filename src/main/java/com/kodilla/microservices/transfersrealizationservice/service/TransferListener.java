abe18799c045fbfa0822f3dedb95dde886aeca15
kacper <kacper.szubra>
2020-10-18 18:58:51 +0200

    first commit

---
+++ src/main/java/com/kodilla/microservices/transfersrealizationservice/service/TransferListener.java
@@ -0,0 +1,24 @@
+package com.kodilla.microservices.transfersrealizationservice.service;
+
+import com.kodilla.microservices.commons.TransferMessage;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Slf4j
+@Service
+public class TransferListener {
+
+    @KafkaListener(topics = "transfers")
+    public void consume(@Payload TransferMessage transferMessage) throws IOException {
+        log.info("Consumed transferMessage: from {} to: {} for: {} PLN",
+                transferMessage.getTransfer().getSenderAccount(),
+                transferMessage.getTransfer().getRecipientAccount(),
+                transferMessage.getTransfer().getAmount()
+        );
+    }
+
+}
